ab0e9af04e31256c20ff89e8e5df07b37270e117
Booglejr <rooty@kwtechllc.com>
2022-01-09 16:44:15 -0600

    initial upload

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.booglejr.demo {
+    requires java.base;
+    requires java.logging;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.controls;
+
+    exports com.booglejr.demo;
+}
